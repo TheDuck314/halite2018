caa5b02ebbc2aca2bec7dfd3fd0f84cdfe13b41c
Greg McGlynn <gem2128@columbia.edu>
2018-11-09 22:34:03 -0500

    add 18_tau from 17_sigma

---
+++ bots/18_tau/hlt/Plan.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <string>
+#include "Player.h"
+#include "Geometry.h"
+#include "StringUtil.h"
+using namespace std;
+
+enum class Purpose
+{
+    Stuck = 0,          // must stay put b/c too little halite
+    Ram = 1,            // trying to hit an adjacent enemy
+    EvadeReturner = 2,  // getting out of the way of someone with the Return purpose
+    Return = 3,         // bringing halite back to base
+    Flee = 4,           // moving away from an adjacent enemy
+    Mine = 5,           // getting halite
+};
+
+string desc(Purpose p);
+
+struct Plan 
+{
+    Ship ship;
+    Vec tgt;
+    Purpose purpose;
+
+    string toString() const { return stringf("Plan(ship=%s tgt=%s purpose=%s", +ship.toString(), +tgt.toString(), +desc(purpose)); }
+};
+
---
+++ bots/18_tau/hlt/PosMap.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <string>
+#include "Geometry.h"
+#include "Grid.h"
+
+template<class T>
+class PosMap final
+{
+  public:
+    explicit PosMap(T filler)
+    {
+        arr = new T[grid.width * grid.height];
+        fill(filler);
+    }
+    PosMap(const PosMap &rhs)
+    {
+        arr = new T[grid.width * grid.height];
+        *this = rhs;
+    }
+    PosMap& operator=(const PosMap &rhs)
+    {
+        std::copy(rhs.arr, rhs.arr + grid.width * grid.height, arr);
+        return *this;
+    }
+    ~PosMap() { delete [] arr; }
+
+    T& operator()(Vec pos) { return arr[grid.width * pos.y + pos.x]; }
+    const T& operator()(Vec pos) const { return const_cast<PosMap<T>*>(this)->operator()(pos); }
+
+    void fill(T filler) { std::fill(arr, arr + grid.width * grid.height, filler); }
+
+  private:
+      T *arr;
+};
+
+using PosSet = PosMap<bool>;
+
+
+string toString(const PosSet &ps);
